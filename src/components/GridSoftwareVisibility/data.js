aa736de352f8836350391d6e736f2b06b916a792
rajesh6161 <rajeshranjan2712000@gmail.com>
2021-04-03 17:17:05 +0530

    done

--- src/components/GridSoftwareVisibility/data.js
+++ src/components/GridSoftwareVisibility/data.js
@@ -1,47 +1,47 @@
 const data = [
   {
     id: 1,
-    image: '/assets/product/p1.jpg',
+    image: '/assets/product/p1.png',
     items: [1, 2, 3],
   },
   {
     id: 2,
-    image: '/assets/product/p2.jpg',
+    image: '/assets/product/p2.png',
     items: [1, 2],
   },
   {
     id: 3,
-    image: '/assets/product/p3.jpg',
+    image: '/assets/product/p3.png',
     items: [1, 2, 3],
   },
   {
     id: 4,
-    image: '/assets/product/p4.jpg',
+    image: '/assets/product/p4.png',
     items: [1, 2, 3, 4],
   },
   {
     id: 5,
-    image: '/assets/product/p5.jpg',
+    image: '/assets/product/p5.png',
     items: [1, 2, 3],
   },
   {
     id: 6,
-    image: '/assets/product/p6.jpg',
+    image: '/assets/product/p6.png',
     items: [1],
   },
   {
     id: 7,
-    image: '/assets/product/p7.jpg',
+    image: '/assets/product/p7.png',
     items: [1, 2],
   },
   {
     id: 8,
-    image: '/assets/product/p8.jpg',
+    image: '/assets/product/p8.png',
     items: [1, 2, 3],
   },
   {
     id: 9,
-    image: '/assets/product/p9.jpg',
+    image: '/assets/product/p9.png',
     items: [1, 2, 3],
   },
 ];
